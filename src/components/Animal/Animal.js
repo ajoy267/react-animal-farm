6e14d41fa4ad4c0d7327476cb22ed351d013f2df
ajoy267 <andrew.joy12@hotmail.com>
2021-12-01 14:26:16 -0800

    destructed aniaml.js

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Animal.css'
 
-export default function Animal(props) {
+export default function Animal({ name, says, top, left, type }) {
   return (
-    <div className="animal" style={{ top: props.top, left: props.left }}>
-      <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
-      <span className="name">{props.name}</span>
-      <span>{props.says}</span>
+    <div className="animal" style={{ top, left }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
     </div>
   )
 }
